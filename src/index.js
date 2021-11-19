58410ad71e260a3229562fa0cae0e232d87cc444
miller2590 <miller2590@gmail.com>
2021-11-19 15:51:42 -0600

    Final product

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Child from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const names = {
+  first: "John",
+  second: "Jake",
+  third: "Ryan",
+  fourth: "Sam",
+};
 
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { src: names.first };
+    this.randomName = this.randomName.bind(this);
+  }
+
+  randomName() {
+    const keys = Object.keys(names);
+    let randKey = keys[Math.floor(Math.random() * keys.length)];
+
+    while (names[randKey] === this.state) {
+      randKey = keys[Math.floor(Math.random() * keys.length)];
+    }
+
+    this.setState({
+      src: names[randKey],
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          My name is <h1>{this.state.src}</h1>
+        </p>
+        <Child randomName={this.randomName} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Parent />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
